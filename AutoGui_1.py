2a1e15fd991a960206bd422f462151b52efcc2f7
mr-st-01-ilja <ilja.erik.krylov@gmail.com>
2025-02-11 09:44:41 +0000

    testing in a visual program.

---
+++ AutoGui_1.py
@@ -0,0 +1,14 @@
+
+import time
+import pyautogui
+
+
+pyautogui.alert(text='Warning! Malware Detected!', title='Windows Malware Protection', button='OK', )
+
+pyautogui.confirm(text='PRESS OK', title='V1RU5', buttons=['OK', 'OK'])
+
+pyautogui.password(text='TYPE YOUR PASSWORD', title='V1RU5', default='', mask='*')
+
+pyautogui.password(text='CONFIRM YOUR PASSWORD', title='V1RU5', default='', mask='*')
+
+pyautogui.alert(text='THANK YOU FOR YOUR COOPERATION', title='V1RU5', button='delete Systen69', )
