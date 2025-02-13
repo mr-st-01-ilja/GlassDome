0382255a6b0cdd7528e91eefb9f46b972bbde943
mr-st-01-ilja <ilja.erik.krylov@gmail.com>
2025-02-13 10:23:33 +0000

    Python auto gui code editing itself

--- AutoGui_1.py
+++ AutoGui_1.py
@@ -1,14 +1,91 @@
-
+import tkinter
 import time
 import pyautogui
+runs = 9
+runs_10 = 0
+
+pyautogui.moveTo(406, 163, duration=0.3)
+time.sleep(0.5)
+pyautogui.dragRel(30, 0, duration=0.3)
+runs += 1
+pyautogui.typewrite(str(runs))
 
 
+if runs % 10 == 0:
+    runs_10 += 1
+    pyautogui.moveRel(-5, 15, duration=0.3)
+    pyautogui.dragRel(30, 00, duration=0.3)
+    pyautogui.typewrite(str(runs_10))
+
+    pyautogui.moveRel(0, 23 * runs_10 - 23, duration=0.3)
+    pyautogui.leftClick()
+    pyautogui.press("enter")
+    pyautogui.typewrite('runs'+ str(runs_10)+'= 0')
+
+if runs % 3 == 0 and not runs <= 10:
+    pyautogui.moveTo(406, 163, duration=0.3)
+    pyautogui.dragRel(10, 0, duration=0.3)
+    pyautogui.hotkey("ctrl", "c")
+    clipboard_content = tkinter.Tk().clipboard_get()
+    print(clipboard_content)
+
+
+
+
+"""
+    if int(clipboard_content) == 1:
+        runs1 += 1
+        pyautogui.moveTo(405, 207, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(int(runs1))
+    if int(clipboard_content) == 2:
+        runs2 += 1
+        pyautogui.moveTo(405, 230, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs2)
+    if int(clipboard_content) == 3:
+        runs3 += 1
+        pyautogui.moveTo(405, 252, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs3)
+    if int(clipboard_content) == 4:
+        runs4 += 1
+        pyautogui.moveTo(405, 274, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs4)
+    if int(clipboard_content) == 5:
+        runs5 += 1
+        pyautogui.moveTo(405, 296, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs5)
+    if int(clipboard_content) == 6:
+        runs6 += 1
+        pyautogui.moveTo(405, 318, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs6)
+    if int(clipboard_content) == 7:
+        runs7 += 1
+        pyautogui.moveTo(405, 340, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs7)
+    if int(clipboard_content) == 8:
+        runs8 += 1
+        pyautogui.moveTo(405, 362, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs8)
+    if int(clipboard_content) == 9:
+        runs9 += 1
+        pyautogui.moveTo(405, 384, duration=0.3)
+        pyautogui.doubleClick()
+        pyautogui.typewrite(runs9)
+"""
+
+"""
 pyautogui.alert(text='Warning! Malware Detected!', title='Windows Malware Protection', button='OK', )
-
 pyautogui.confirm(text='PRESS OK', title='V1RU5', buttons=['OK', 'OK'])
-
 pyautogui.password(text='TYPE YOUR PASSWORD', title='V1RU5', default='', mask='*')
-
 pyautogui.password(text='CONFIRM YOUR PASSWORD', title='V1RU5', default='', mask='*')
-
 pyautogui.alert(text='THANK YOU FOR YOUR COOPERATION', title='V1RU5', button='delete Systen69', )
+"""
+
+
