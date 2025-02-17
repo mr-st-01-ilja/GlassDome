19b6b27681aed8e520187328111e09b45bbac2cb
mr-st-01-ilja <ilja.erik.krylov@gmail.com>
2025-02-17 15:35:12 +0000

    Python challanges

---
+++ Python_coding_puzzles.py
@@ -0,0 +1,150 @@
+import tkinter
+import time
+import pyautogui as pya
+import math
+import random
+
+
+
+
+""" 16
+My_Var1 = 48
+
+while My_Var1 > 9:
+    x = 0
+    y = 0
+    while My_Var1 > 0:
+        if My_Var1 >= 10:
+            My_Var1 = My_Var1 - 10
+            x += 1
+        elif My_Var1 >= 1:
+            My_Var1 = My_Var1 - 1
+            y += 1
+    My_Var1 = y+x
+    print(My_Var1)
+"""
+
+
+""" 12/13/14/15
+My_Set1 = [1, 2, 5, 1, 2, 4]
+for n in My_Set1:
+    x = My_Set1.count(n)
+    if x == 1:
+        print(n)
+"""
+
+
+""" 24
+n = 0
+while n < 12:
+    n += 1
+    if n % 2 == 0:
+        print(n)
+    elif n % 3 == 0:
+        print(n)
+    elif n % 5 == 0:
+        print(n)
+"""
+
+
+""" 23
+n = 111
+step1 = 0
+while n > 1:
+    if n % 2 == 0:
+        n = n / 2
+        print(n)
+        step1 += 1
+    else:
+        n = n * 3 + 1
+        print(n)
+        step1 += 1
+    if n == 1:
+        print(n)
+        print(step1)
+        break
+"""
+
+
+
+""" 7/8
+My_Set1 = {1, 2, 4, 5, 6, 8}
+
+My_Var1 = 0
+for n in My_Set1:
+    My_Var1 += 1
+    if n != My_Var1:
+        print(My_Var1)
+        My_Var1 = n
+"""
+
+
+""" 18----------
+My_Var1 = 64
+My_Var1 = My_Var1 * -1
+print(My_Var1)
+"""
+
+
+""" 11
+My_Var1 = 26
+
+My_Var2 = math.sqrt(My_Var1)
+My_Var3 = int(My_Var2)
+if My_Var3 == My_Var2:
+    print(My_Var3)
+else:
+    print(False)
+"""
+
+
+""" 5/6
+My_Var1 = 16
+My_Var2 = 4
+if My_Var1 % My_Var2 == 0:
+    print(True)
+else:
+    print(False)
+"""
+
+
+""" 4
+My_Var1 = 25
+
+My_Var2 = math.sqrt(My_Var1)
+My_Var3 = int(My_Var2)
+if My_Var3 == My_Var2:
+    print(True)
+else:
+    print(False)
+"""
+
+
+""" 3
+My_Var1 = 8
+
+if My_Var1 % 4 == 0:
+    print(True)
+else:
+    print(False)
+"""
+
+
+""" 2
+My_Var1 = 12
+
+if My_Var1 % 3 == 0:
+    print(True)
+else:
+    print(False)
+"""
+
+
+""" 1
+My_Var1 = 6
+
+if My_Var1 % 2 == 0:
+    print(True)
+else:
+    print(False)
+"""
