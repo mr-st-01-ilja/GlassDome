ee373d97c9520e45129ee159a9be292be1a66820
mr-st-01-ilja <ilja.erik.krylov@gmail.com>
2025-02-18 12:05:48 +0000

    Changing database with Python

--- DataBase_something.py
+++ DataBase_something.py
@@ -1,16 +1,32 @@
-import sqlite3 as sql
-
 import psycopg2
 
-connection = psycopg2.connect(database="dbname", user="username", password="pass", host="hostname", port=5432)
+
+
+connection = psycopg2.connect(database="postgres", user="postgres", password="", host="localhost", port=5432)
 
 cursor = connection.cursor()
 
-cursor.execute("SELECT * from portal.portal_users;")
 
-# Fetch all rows from database
-record = cursor.fetchall()
 
-print("Data from Database:- ", record)
+#cursor.execute("INSERT Into persons(personid, lastname, firstname, address, city) VALUES('1970','Godly','Stalin','Homeless_street','Moscow')");
+
+#cursor.execute("UPDATE persons SET lastname = 'French' WHERE personid = '2763';")
+#cursor.execute("UPDATE persons SET firstname = 'Baguette' WHERE personid = '2763';")
+#cursor.execute("UPDATE persons SET address = 'Tower_street' WHERE personid = '2763';")
+#cursor.execute("UPDATE persons SET city = 'France' WHERE personid = '2763';")
+
+cursor.execute('SELECT * FROM persons ORDER BY personid')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+
+connection.commit()
+connection.close()
+
+#cursor.execute("SELECT * from persons.;")
+# record = cursor.fetchall()
+
+#print("Data from Database:- ", record)
 
 
