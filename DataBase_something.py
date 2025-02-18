37070b60317def4ad1efc083c46a36dff34294da
mr-st-01-ilja <ilja.erik.krylov@gmail.com>
2025-02-18 10:16:52 +0000

    Python challanges

---
+++ DataBase_something.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+
+import psycopg2
+
+connection = psycopg2.connect(database="dbname", user="username", password="pass", host="hostname", port=5432)
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * from portal.portal_users;")
+
+# Fetch all rows from database
+record = cursor.fetchall()
+
+print("Data from Database:- ", record)
+
+
