de5f74ffe1777def4457520eca7a67004089a052
mr-st-01-ilja <ilja.erik.krylov@gmail.com>
2025-02-21 13:20:30 +0000

    Changing database with Python

--- DataBase_something.py
+++ DataBase_something.py
@@ -3,30 +3,23 @@ import psycopg2
 
 
 connection = psycopg2.connect(database="postgres", user="postgres", password="", host="localhost", port=5432)
-
 cursor = connection.cursor()
 
 
+#cursor.execute("CREATE TABLE datacamp_courses(course_id SERIAL PRIMARY KEY,course_name VARCHAR (50) UNIQUE NOT NULL,course_instructor VARCHAR (100) NOT NULL,topic VARCHAR (20) NOT NULL)")
 
-#cursor.execute("INSERT Into persons(personid, lastname, firstname, address, city) VALUES('1970','Godly','Stalin','Homeless_street','Moscow')");
+#cursor.execute("INSERT Into persons(personid, lastname, firstname, address, city) VALUES('1970','Godly','Stalin','Homeless_street','Moscow')")
 
-#cursor.execute("UPDATE persons SET lastname = 'French' WHERE personid = '2763';")
-#cursor.execute("UPDATE persons SET firstname = 'Baguette' WHERE personid = '2763';")
-#cursor.execute("UPDATE persons SET address = 'Tower_street' WHERE personid = '2763';")
-#cursor.execute("UPDATE persons SET city = 'France' WHERE personid = '2763';")
+#cursor.execute("UPDATE persons SET lastname = 'French' WHERE personid = '2763'")
 
-cursor.execute('SELECT * FROM persons ORDER BY personid')
+
+cursor.execute("SELECT lastname,firstname,salary FROM persons WHERE salary != 10000 AND salary != 15000")
+
 rows = cursor.fetchall()
 for row in rows:
     print(row)
 
 
-connection.commit()
+#connection.commit()
+cursor.close()
 connection.close()
-
-#cursor.execute("SELECT * from persons.;")
-# record = cursor.fetchall()
-
-#print("Data from Database:- ", record)
-
-
